6a80b7964c03fa92952beda226ed26e972249679
George <gdewey@ucla.edu>
2020-01-22 03:14:49 +0000

    Got filenames to print out right

--- hw1/autoSim_4.1.R
+++ hw1/autoSim_4.1.R
@@ -1,8 +1,19 @@
 # autoSim.R
 
-nVals <- seq(10000, 10000, by=10000)
-for (n in nVals) {
-  oFile <- paste("n", n, ".txt", sep="")
-  sysCall <- paste("nohup Rscript runSim.R n=", n, " > ", oFile, sep="")
-  system(sysCall, wait=F)
-  print(paste("sysCall=", sysCall, sep=""))
+for (arg in commandArgs(TRUE)) {
+  eval(parse(text=arg))
+}
+
+nVals = seq(100, 500, by=100)
+distTypes = c("gaussian", "t1", "t5")
+
+for (dist in distTypes){
+  for (n in nVals) {
+    arg = paste("n=", n, " dist=", shQuote(shQuote(dist)), " seed=", seed, " reps=", rep, sep="")
+    oFile = paste("n", n, "_dist_", dist), ".txt", sep="")
+    sysCall = paste("nohup Rscript runSim_4.1.R ", arg, " > ", oFile, sep="")
+    system(sysCall, wait=F)
+    print(paste("sysCall=", sysCall, sep=""))
+  }
+}
+

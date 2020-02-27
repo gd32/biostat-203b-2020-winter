32a908bd415d6a39f0f02d4ebd14df962392b8aa
George Dewey <gdewey@ucla.edu>
2020-02-27 07:36:49 +0000

    Added province level barplots

--- hw3/covid_map/app.R
+++ hw3/covid_map/app.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
@@ -11,10 +10,12 @@
 library(shiny)
 library(tidyverse)
 library(lubridate)
+library(grid)
+library(gridExtra)
 
 setwd('/home/gdewey/biostat-203b-2020-winter/hw3')
 
-# Define UI for application that draws a histogram
+# Define UI 
 ui = fluidPage(
 
     # Application title
@@ -23,10 +24,54 @@ ui = fluidPage(
     # Date input
     dateInput(inputId = "date", 
               label = "Date", 
-              value = "2020-02-25",
+              value = "2020-02-24",
               min = "2020-01-22",
-              max = "2020-02-25"),
+              max = "2020-02-24"),
     
+    # Case type input
+    selectInput(inputId = "casetype",
+                label = "Case Type",
+                choices = c("Confirmed", "Recovered", "Death")),
+    
+    
+    # Province input (for sideplot)
+    selectInput(inputId = "province",
+                label = "Province",
+                choices = c("Anhui",
+                            "Fujian",
+                            "Gansu",
+                            "Guangdong",
+                            "Guizhou",
+                            "Hainan",
+                            "Hebei",
+                            "Heilongjiang",
+                            "Henan",
+                            "Hubei",
+                            "Hunan",
+                            "Jiangsu",
+                            "Jiangxi",
+                            "Jilin",
+                            "Liaoning",
+                            "Qinghai",
+                            "Shaanxi",
+                            "Shandong",
+                            "Shanxi",
+                            "Sichuan",
+                            "Yunnan",
+                            "Zhejiang",
+                            "Guangxi",
+                            "Inner Mongolia",
+                            "Ningxia",
+                            "Xinjiang",
+                            "Tibet",
+                            "Beijing",
+                            "Chongqing",
+                            "Shanghai",
+                            "Tianjin",
+                            "Hong Kong",
+                            "Macau")),
+    
+    # Main output will be incidence map
     mainPanel(plotOutput("chnmap"))
         
 )
@@ -35,34 +80,52 @@ ui = fluidPage(
 covid = read_csv("covid.csv")
 covid
 
+
 provs = read_csv("provs.csv")
 provs
 
 # Define server logic
 server = function(input, output) {
+    
     dateInput = reactive({input$date})
-    count = "confirmed"
-    output$chnmap = renderPlot({covid %>%
-        filter(`Country/Region` %in% c("Mainland China", 
-                                       "Macau", 
-                                       "Hong Kong", 
-                                       "Taiwan")) %>%
-        filter(date == dateInput()) %>%
-        group_by(`Province/State`) %>%
-        top_n(1, date) %>% 
-        right_join(chn_prov, by = c("Province/State" = "NAME_ENG")) %>%
-        ggplot() +
-        geom_sf(mapping = aes(fill = !!sym(count), geometry = geometry)) +
-        scale_fill_gradient(name = "Cases", 
-                            trans = "log10", 
-                            breaks = c(5, 50, 500, 5000, 50000),
-                            labels = c(5, 50, 500, 5000, 50000),
-                            guide = "legend",
-                            low = "lightblue",
-                            high = "red") +
-        theme_bw() +
-        labs(title = str_c(count, " cases"), subtitle = dateInput())})
-}
+    
+    outcome = reactive({input$casetype})
+    
+    province = reactive({input$province})
+
+    output$chnmap = renderPlot({
+        p1 = covid %>%
+            filter(`Country/Region` %in% c("Mainland China",
+                                           "Macau",
+                                           "Hong Kong",
+                                           "Taiwan")) %>%
+            filter(date == dateInput()) %>%
+            group_by(`Province/State`) %>%
+            top_n(1, date) %>%
+            right_join(chn_prov, by = c("Province/State" = "NAME_ENG")) %>%
+            ggplot() +
+            geom_sf(mapping = aes(fill = !!sym(tolower(outcome())), 
+                                  geometry = geometry)) +
+            scale_fill_gradient(name = "Count",
+                                trans = "log10",
+                                breaks = c(5, 50, 500, 5000, 50000),
+                                labels = c(5, 50, 500, 5000, 50000),
+                                guide = "legend",
+                                low = "blue",
+                                high = "orange") +
+            theme_bw() +
+            
+            labs(title = str_c(outcome(), " Cases"), subtitle = dateInput())
+        
+        p2 = covid %>% filter(`Province/State` == province()) %>% 
+             ggplot() + 
+             geom_col(aes(date, eval(parse(text = tolower(outcome()))))) +
+             xlab("Date") + 
+             ylab("Count")
+   
+        grid.arrange(p1, p2, nrow=1)}
+        )
+ }
 
 # Run the application 
 shinyApp(ui = ui, server = server)

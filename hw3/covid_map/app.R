fc1b3a7cdf56e8ad5dd377831a3581a2e48115e9
George Dewey <gdewey@ucla.edu>
2020-02-28 23:03:54 +0000

    Added theme, prototype is live

--- hw3/covid_map/app.R
+++ hw3/covid_map/app.R
@@ -5,23 +5,112 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(tidyverse)
 library(lubridate)
-library(grid)
-library(gridExtra)
+library(sf)
+library(shinythemes)
 
-setwd('/home/gdewey/biostat-203b-2020-winter/hw3')
+covid = read_csv("covid.csv")
+reddit = read_csv("reddit.csv")
+
+chn_map = st_read("bou2_4p.shp", as_tibble = TRUE) %>%
+    mutate(NAME = iconv(NAME, from = "GBK"),
+           BOU2_4M_ = as.integer(BOU2_4M_),
+           BOU2_4M_ID = as.integer(BOU2_4M_ID)) %>%
+    mutate(NAME = str_replace_na(NAME, replacement = "澳门特别行政区"))
+
+translate <- function(x) {
+    sapply(x, function(chn_name) {
+        if (str_detect(chn_name, "澳门")) {
+            eng_name <- "Macau"
+        } else if (str_detect(chn_name, "台湾")) {
+            eng_name <- "Taiwan"
+        } else if (str_detect(chn_name, "上海")) {
+            eng_name <- "Shanghai"
+        } else if (str_detect(chn_name, "云南")) {
+            eng_name <- "Yunnan"
+        } else if (str_detect(chn_name, "内蒙古")) {
+            eng_name <- "Inner Mongolia"
+        } else if (str_detect(chn_name, "北京")) {
+            eng_name <- "Beijing"
+        } else if (str_detect(chn_name, "台湾")) {
+            eng_name <- "Taiwan"
+        } else if (str_detect(chn_name, "吉林")) {
+            eng_name <- "Jilin"
+        } else if (str_detect(chn_name, "四川")) {
+            eng_name <- "Sichuan"
+        } else if (str_detect(chn_name, "天津")) {
+            eng_name <- "Tianjin"
+        } else if (str_detect(chn_name, "宁夏")) {
+            eng_name <- "Ningxia"
+        } else if (str_detect(chn_name, "安徽")) {
+            eng_name <- "Anhui"
+        } else if (str_detect(chn_name, "山东")) {
+            eng_name <- "Shandong"
+        } else if (str_detect(chn_name, "山西")) {
+            eng_name <- "Shanxi"
+        } else if (str_detect(chn_name, "广东")) {
+            eng_name <- "Guangdong"
+        } else if (str_detect(chn_name, "广西")) {
+            eng_name <- "Guangxi"
+        } else if (str_detect(chn_name, "新疆")) {
+            eng_name <- "Xinjiang"
+        } else if (str_detect(chn_name, "江苏")) {
+            eng_name <- "Jiangsu"
+        } else if (str_detect(chn_name, "江西")) {
+            eng_name <- "Jiangxi"
+        } else if (str_detect(chn_name, "河北")) {
+            eng_name <- "Hebei"
+        } else if (str_detect(chn_name, "河南")) {
+            eng_name <- "Henan"
+        } else if (str_detect(chn_name, "浙江")) {
+            eng_name <- "Zhejiang"
+        } else if (str_detect(chn_name, "海南")) {
+            eng_name <- "Hainan"
+        } else if (str_detect(chn_name, "湖北")) {
+            eng_name <- "Hubei"
+        } else if (str_detect(chn_name, "湖南")) {
+            eng_name <- "Hunan"
+        } else if (str_detect(chn_name, "甘肃")) {
+            eng_name <- "Gansu"
+        } else if (str_detect(chn_name, "福建")) {
+            eng_name <- "Fujian"
+        } else if (str_detect(chn_name, "西藏")) {
+            eng_name <- "Tibet"
+        } else if (str_detect(chn_name, "贵州")) {
+            eng_name <- "Guizhou"
+        } else if (str_detect(chn_name, "辽宁")) {
+            eng_name <- "Liaoning"
+        } else if (str_detect(chn_name, "重庆")) {
+            eng_name <- "Chongqing"
+        } else if (str_detect(chn_name, "陕西")) {
+            eng_name <- "Shanxi"
+        } else if (str_detect(chn_name, "青海")) {
+            eng_name <- "Qinghai"
+        } else if (str_detect(chn_name, "香港")) {
+            eng_name <- "Hong Kong"
+        } else if (str_detect(chn_name, "黑龙江")) {
+            eng_name <- "Heilongjiang"
+        } else {
+            eng_name <- chn_name # don't translate if no correspondence
+        }
+        return(eng_name)
+    })
+}
+
+
+chn_prov = chn_map %>% 
+    count(NAME) %>%
+    mutate(NAME_ENG = translate(NAME)) # translate function is vectorized
 
 # Define UI 
-ui = fluidPage(
+ui = fluidPage(theme = shinytheme("paper"),
     
     # Application title
-    titlePanel(h1("Visualizing the 2020 Coronavirus Epidemic")),
+    titlePanel("Visualizing the 2020 Coronavirus Epidemic"),
     
-    headerPanel(""),
     headerPanel(""),
     
     # Sidebar
@@ -31,7 +120,7 @@ ui = fluidPage(
         dateInput(inputId = "date", 
                   label = "Date", 
                   value = "2020-02-26",
-                  min = "2020-01-22",
+                  min = "2020-01-23",
                   max = "2020-02-26"),
     
         # Case type input
@@ -48,17 +137,6 @@ ui = fluidPage(
                     tabPanel("Reddit posts", tableOutput("Reddit")))),
 
 )
-
-# Load virus and map data
-covid = read_csv("covid.csv")
-covid
-
-provs = read_csv("provs.csv")
-provs
-
-# Load text data
-reddit = read_csv("reddit.csv")
-reddit
 
 # Define server logic
 server = function(input, output) {
@@ -91,15 +169,23 @@ server = function(input, output) {
                                 low = "blue",
                                 high = "orange") +
             theme_bw() +
-            labs(title = str_c(outcome(), " Cases"), subtitle = dateInput())
-    }) 
+            labs(title = str_c(outcome(), " Cases"), subtitle = dateInput()) + 
+            theme(
+                plot.title = element_text(size = 36, face = "bold"),
+                plot.subtitle = element_text(size = 24),
+                legend.text = element_text(size = 20),
+                legend.title = element_text(size = 24)
+            )
+        
+    }, width = 1200, height = 1000) 
     
     output$prov = renderPlot({
         
        covid %>% filter(`Country/Region` %in% c("Mainland China", 
                                                 "Macau", 
                                                 "Hong Kong", 
-                                                "Taiwan")) %>% 
+                                                "Taiwan"), 
+                        date <= dateInput()) %>% 
                 mutate(isHubei = ifelse(`Province/State` == "Hubei", 
                                         "Hubei", "Others")) %>%
                 ggplot() + 
@@ -108,19 +194,31 @@ server = function(input, output) {
                 labs(fill = "Province") +
                 xlab("Date") +
                 ylab("Count") +
+                labs(title = str_c("\n", outcome(), " Cases"), 
+                     subtitle = str_c("Cumulative until ", dateInput())) +
                 scale_fill_manual(values = c("orange", "blue")) +
-                theme_bw()
+                theme_bw() +
+                theme(plot.title = element_text(size = 36, face = "bold"),
+                      plot.subtitle = element_text(size = 24),
+                      axis.text = element_text(size = 18),
+                      axis.title = element_text(size = 24),
+                      legend.text = element_text(size = 18),
+                      legend.title = element_text(size = 20)
+            )
     
-        })
+        }, width = 1000, height = 800)
     
     output$Reddit = renderTable({
         
         s1 = reddit %>% filter(sub == "Science", 
-                               created_date == ymd(dateInput())) %>% sample_n(5) 
+                               created_date == ymd(dateInput())) %>% 
+                               sample_n(1) 
         s2 = reddit %>% filter(sub == "News", 
-                               created_date == ymd(dateInput())) %>% sample_n(5)
+                               created_date == ymd(dateInput())) %>% 
+                               sample_n(1)
         s3 = reddit %>% filter(sub == "Coronavirus", 
-                               created_date == ymd(dateInput())) %>% sample_n(5)
+                               created_date == ymd(dateInput())) %>% 
+                               sample_n(3)
         
         display_table = bind_rows(s1, s2, s3)
         
